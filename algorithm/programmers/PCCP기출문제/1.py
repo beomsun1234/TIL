06c8113a7bcd82823432f8f9e08bd69161b0eaa1
Park Beomsun <beomsun22@gmail.com>
2023-12-28 21:17:01 +0900

    Update 1.py

--- algorithm/programmers/PCCP기출문제/1.py
+++ algorithm/programmers/PCCP기출문제/1.py
@@ -1,1 +1,34 @@
-#1
+# 1번
+def solution(bandage, health, attacks):
+    answer = 0
+    nH = health
+    time = 0
+    end = attacks[-1][0]
+    band_cnt = 0
+    while time < end:
+        time +=1
+        # 공격
+        if attacks[0][0] == time:
+            nH -= attacks[0][1]
+            attacks.pop(0)
+            band_cnt = 0
+            if nH <=0 :
+                break
+        else:
+            if band_cnt > bandage[0]:
+                band_cnt = 0
+                continue
+            band_cnt+=1
+            heel = bandage[1]
+            if band_cnt == bandage[0]:
+                heel += bandage[2]
+                band_cnt =0
+            nH += heel
+            
+            if nH > health:
+                nH = health
+
+    if nH <=0:
+        nH = -1
+      
+    return nH
